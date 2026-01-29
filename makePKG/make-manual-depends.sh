77dad265b2c9fd1527ca19b16c5ee8d1d6350e5c
Ordinary-Ladess <unferium@gmail.com>
2026-01-29 11:59:22 +0000

    add makePKG dir with placeholder dependencies

---
+++ makePKG/make-manual-depends.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./build-logic soc-qcom-sc7280
+./build-logic soc-qcom-sc7280-qbootctl
---
+++ makePKG/soc-qcom-sc7280-qbootctl/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Sorry,\nqbootctl is a placeholder program from a placeholder package.\nIt is to bypass a dependency issue.\n");
+	return 0;
+}
